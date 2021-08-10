b087e006e213e9c638ed84bd4edfa405efeb632f
jackson6868 <tutus@localhost.com>
2021-08-10 13:09:37 +0200

    add script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ docker push jackson3232/multi-worker:latest
 docker push jackson3232/multi-client:$SHA
 docker push jackson3232/multi-server:$SHA
 docker push jackson3232/multi-worker:$SHA
-kubect apply -f k8s
-kubect set image deployments/server-deployment server=jackson3232/multi-server:$SHA
-kubect set image deployments/client-deployment client=jackson3232/multi-client:$SHA
-kubect set image deployments/worker-deployment worker=jackson3232/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=jackson3232/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jackson3232/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jackson3232/multi-worker:$SHA
